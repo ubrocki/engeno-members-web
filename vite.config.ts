11cda6c09c38ebba7f5ebaa2d185d97173d26f2a
Ulrich Brockerhoff <ubrockerhoff@hotmail.de>
2024-06-02 20:51:32 +0200

    Made base dependent on build env

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/engeno-members-web/",
+  base: process.env.NODE_ENV === "production" ? "/engeno-members-web/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
