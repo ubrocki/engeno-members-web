30f1b2cb68088605d23b4ce4cb80de7cd53c825d
Ulrich Brockerhoff <ubrockerhoff@hotmail.de>
2024-06-02 21:00:34 +0200

    Just added some exclamation marks

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ interface HomePageProps {}
 const HomePage: React.FC<HomePageProps> = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>Welcome to the Home Page !!!!</h1>
     </div>
   );
 };
